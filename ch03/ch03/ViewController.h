73cc41db9386cef64c9b8df2de612719e0a02d1e
tyamamo <tyamamo@sneezy.comets.local>
2014-04-03 10:49:56 +0900

    initial commit

---
+++ ch03/ch03/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  ch03
+//
+//  Created by tyamamo on 2014/04/03.
+//  Copyright (c) 2014年 ___FULLUSERNAME___. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
---
+++ ch03/ch03Tests/ch03Tests.m
@@ -0,0 +1,34 @@
+//
+//  ch03Tests.m
+//  ch03Tests
+//
+//  Created by tyamamo on 2014/04/03.
+//  Copyright (c) 2014年 ___FULLUSERNAME___. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface ch03Tests : XCTestCase
+
+@end
+
+@implementation ch03Tests
+
+- (void)setUp
+{
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown
+{
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    XCTFail(@"No implementation for \"%s\"", __PRETTY_FUNCTION__);
+}
+
+@end
