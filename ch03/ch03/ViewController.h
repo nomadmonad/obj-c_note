22f69d185483ce2c8cdf1ae4e57ee2457e765063
nomadmonad <nomadmonad@gmail.com>
2014-04-03 13:36:49 +0900

    Switch、ボタン等の宣言を追加

--- ch03/ch03/ViewController.h
+++ ch03/ch03/ViewController.h
@@ -9,5 +9,8 @@
 #import <UIKit/UIKit.h>
 
 @interface ViewController : UIViewController
+@property (weak, nonatomic) IBOutlet UILabel *ResultField;
+- (IBAction)switchOnOff:(id)sender;
+- (IBAction)ArraySetupButton:(id)sender;
 
 @end
