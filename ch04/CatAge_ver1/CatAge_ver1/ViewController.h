ab3e33557a47f75153574c39fdf32540a3b340fe
nomadmonad <nomadmonad@gmail.com>
2014-04-04 14:23:59 +0900

    アウトレットとアクションを追加

---
+++ ch04/CatAge_ver1/CatAge_ver1/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  CatAge_ver1
+//
+//  Created by tyamamo on 2014/04/04.
+//  Copyright (c) 2014年 ___FULLUSERNAME___. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
