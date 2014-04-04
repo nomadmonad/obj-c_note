a9ee1b66d6c2fdb8a38276cede8630c8e7f5c827
nomadmonad <nomadmonad@gmail.com>
2014-04-04 16:31:51 +0900

    ex_UtilityApplication initial commit

---
+++ ch05/ex_UtilityApplication/ex_UtilityApplication/MainViewController.h
@@ -0,0 +1,13 @@
+//
+//  MainViewController.h
+//  ex_UtilityApplication
+//
+//  Created by tyamamo on 2014/04/04.
+//  Copyright (c) 2014年 ___FULLUSERNAME___. All rights reserved.
+//
+
+#import "FlipsideViewController.h"
+
+@interface MainViewController : UIViewController <FlipsideViewControllerDelegate>
+
+@end
