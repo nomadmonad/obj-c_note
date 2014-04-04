88be6ee2464a059afe04aba56cd59b98ae2d4b34
nomadmonad <nomadmonad@gmail.com>
2014-04-04 11:38:29 +0900

    IBActionのサンプル initial commit

---
+++ ch04/ex_IBOutlets/ex_IBAction1Tests/ex_IBAction1Tests.m
@@ -0,0 +1,34 @@
+//
+//  ex_IBAction1Tests.m
+//  ex_IBAction1Tests
+//
+//  Created by tyamamo on 2014/04/04.
+//  Copyright (c) 2014年 ___FULLUSERNAME___. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface ex_IBAction1Tests : XCTestCase
+
+@end
+
+@implementation ex_IBAction1Tests
+
+- (void)setUp
+{
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown
+{
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    XCTFail(@"No implementation for \"%s\"", __PRETTY_FUNCTION__);
+}
+
+@end
