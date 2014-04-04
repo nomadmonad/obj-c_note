f498d841f3723ea47d0accf1b00b5f81ed52e24e
nomadmonad <nomadmonad@gmail.com>
2014-04-04 10:32:25 +0900

    ch04 ex_SingleView initial Commit

---
+++ ch04/ex_SingleView/ex_SingleView/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  ex_SingleView
+//
+//  Created by tyamamo on 2014/04/04.
+//  Copyright (c) 2014年 ___FULLUSERNAME___. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
