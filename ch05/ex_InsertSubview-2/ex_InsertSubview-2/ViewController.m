99ddf1bc617ce6b92532f539c849e850b1b3397b
nomadmonad <nomadmonad@gmail.com>
2014-04-07 11:32:42 +0900

    SubViewの順序を入れ替えるサンプル

---
+++ ch05/ex_InsertSubview-2/ex_InsertSubview-2/ViewController.m
@@ -0,0 +1,36 @@
+//
+//  ViewController.m
+//  ex_InsertSubview-2
+//
+//  Created by tyamamo on 2014/04/07.
+//  Copyright (c) 2014年 ___FULLUSERNAME___. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+@property (strong, nonatomic) IBOutlet UIImageView *dai;
+- (IBAction)valueChanged:(UIStepper *)sender;
+
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+	// Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+
+- (IBAction)valueChanged:(UIStepper *)sender {
+    [self.view insertSubview:_dai atIndex:sender.value];
+}
+@end
---
+++ ch05/ex_InsertSubview-2/ex_InsertSubview-2Tests/ex_InsertSubview_2Tests.m
@@ -0,0 +1,34 @@
+//
+//  ex_InsertSubview_2Tests.m
+//  ex_InsertSubview-2Tests
+//
+//  Created by tyamamo on 2014/04/07.
+//  Copyright (c) 2014年 ___FULLUSERNAME___. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface ex_InsertSubview_2Tests : XCTestCase
+
+@end
+
+@implementation ex_InsertSubview_2Tests
+
+- (void)setUp
+{
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown
+{
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    XCTFail(@"No implementation for \"%s\"", __PRETTY_FUNCTION__);
+}
+
+@end
