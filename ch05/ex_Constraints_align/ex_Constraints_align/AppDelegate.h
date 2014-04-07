16119cacd9dd6152b892da61fc888053ce731502
nomadmonad <nomadmonad@gmail.com>
2014-04-07 13:49:22 +0900

    ConstraintsでAlignを設定するサンプル

---
+++ ch05/ex_Constraints_align/ex_Constraints_align/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  ex_Constraints_align
+//
+//  Created by tyamamo on 2014/04/07.
+//  Copyright (c) 2014年 ___FULLUSERNAME___. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
