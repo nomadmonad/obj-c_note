32bcee3184127fbbc90def257555fefa788aa176
nomadmonad <nomadmonad@gmail.com>
2014-04-07 11:07:01 +0900

    ビューの挿入を実施する変更を加えてみる

--- ch05/ex_InsertSubview/ex_InsertSubview/ViewController.m
+++ ch05/ex_InsertSubview/ex_InsertSubview/ViewController.m
@@ -9,6 +9,11 @@
 #import "ViewController.h"
 
 @interface ViewController ()
+{
+    UIImageView *house;
+}
+@property (strong, nonatomic) IBOutlet UIView *field;
+- (IBAction)insertHouse:(id)sender;
 
 @end
 
@@ -18,6 +23,13 @@
 {
     [super viewDidLoad];
 	// Do any additional setup after loading the view, typically from a nib.
+    UIImage *houseImage = [UIImage imageNamed:@"house.png"];
+    CGRect rect = CGRectMake(0, 0, houseImage.size.width, houseImage.size.height);
+    house = [[UIImageView alloc] initWithFrame:rect];
+    house.image = houseImage;
+    house.contentMode = UIViewContentModeTopLeft;
+    house.center = CGPointMake(68 + (houseImage.size.width / 2), 67 + (houseImage.size.height / 2));
+    
 }
 
 - (void)didReceiveMemoryWarning
@@ -26,4 +38,7 @@
     // Dispose of any resources that can be recreated.
 }
 
+- (IBAction)insertHouse:(id)sender {
+    [_field insertSubview:house atIndex:1];
+}
 @end
