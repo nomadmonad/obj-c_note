21280279712c963f1baf51f399f5dee0912cfa99
nomadmonad <nomadmonad@gmail.com>
2014-04-07 15:33:37 +0900

    ボタンの背景画像のサンプル実装

--- ch05/ex_UIButton_image/ex_UIButton_image/ViewController.m
+++ ch05/ex_UIButton_image/ex_UIButton_image/ViewController.m
@@ -9,7 +9,7 @@
 #import "ViewController.h"
 
 @interface ViewController ()
-
+- (void)hello:(UIButton *)sender;
 @end
 
 @implementation ViewController
@@ -18,6 +18,25 @@
 {
     [super viewDidLoad];
 	// Do any additional setup after loading the view, typically from a nib.
+    
+    UIButton *button = [UIButton buttonWithType:UIButtonTypeCustom];
+    button.frame = CGRectMake(50, 50, 100, 40);
+    
+    button.titleLabel.font = [UIFont boldSystemFontOfSize:18];
+    [button setTitle:@"Hello" forState:UIControlStateNormal];
+    [button setTitleColor:[UIColor whiteColor] forState:UIControlStateNormal];
+    [button setTitleShadowColor:[UIColor darkGrayColor] forState:UIControlStateNormal];
+    UILabel *label = button.titleLabel;
+    label.shadowOffset = CGSizeMake(2, 2);
+    
+    UIImage *buttonImage = [UIImage imageNamed:@"roundbtn.png"];
+    UIImage *resizableImage = [buttonImage resizableImageWithCapInsets:UIEdgeInsetsMake(20, 24, 20, 24)];
+    [button setBackgroundImage:resizableImage forState:UIControlStateNormal];
+    [button setShowsTouchWhenHighlighted:YES];
+    
+    [button addTarget:self action:@selector(hello:) forControlEvents:UIControlEventTouchUpInside];
+    [self.view addSubview:button];
+    
 }
 
 - (void)didReceiveMemoryWarning
@@ -26,4 +45,9 @@
     // Dispose of any resources that can be recreated.
 }
 
+- (void)hello:(UIButton *)sender
+{
+    NSLog(@"hello");
+}
+     
 @end
