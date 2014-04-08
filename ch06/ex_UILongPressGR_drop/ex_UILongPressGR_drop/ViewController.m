62f22d37be08b3f84eeccfbbedaddbaee3dd56f0
nomadmonad <nomadmonad@gmail.com>
2014-04-08 16:29:49 +0900

    Long Pressのイベントからタップされた座標を取得し、その位置に画像をアニメーションさせる処理を実装

--- ch06/ex_UILongPressGR_drop/ex_UILongPressGR_drop/ViewController.m
+++ ch06/ex_UILongPressGR_drop/ex_UILongPressGR_drop/ViewController.m
@@ -9,6 +9,9 @@
 #import "ViewController.h"
 
 @interface ViewController ()
+@property (strong, nonatomic) IBOutlet UIImageView *arrow;
+
+- (IBAction)longPress:(UILongPressGestureRecognizer *)sender;
 
 @end
 
@@ -18,6 +21,7 @@
 {
     [super viewDidLoad];
 	// Do any additional setup after loading the view, typically from a nib.
+    _arrow.hidden = YES;
 }
 
 - (void)didReceiveMemoryWarning
@@ -26,4 +30,19 @@
     // Dispose of any resources that can be recreated.
 }
 
+- (IBAction)longPress:(UILongPressGestureRecognizer *)sender {
+    if (sender.state == UIGestureRecognizerStateBegan) {
+        _arrow.hidden = NO;
+        CGPoint pt = [sender locationInView:self.view];
+        CGFloat height = _arrow.bounds.size.height;
+        _arrow.center = CGPointMake(pt.x, -50);
+        [UIView animateWithDuration:0.5
+                              delay:0.0
+                            options:UIViewAnimationOptionCurveEaseOut
+                         animations:^{
+                             _arrow.center = CGPointMake(pt.x, pt.y - height / 2);
+                         }
+                         completion:nil];
+    }
+}
 @end
