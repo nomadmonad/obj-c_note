2e90527ea2006a284dff918b70ed1ed158711ad5
nomadmonad <nomadmonad@gmail.com>
2014-04-08 15:39:15 +0900

    ピンチインで拡大・縮小するサンプル initial commit

---
+++ ch06/ex_UIPinchGR/ex_UIPinchGR/ViewController.m
@@ -0,0 +1,34 @@
+//
+//  ViewController.m
+//  ex_UIPinchGR
+//
+//  Created by tyamamo on 2014/04/08.
+//  Copyright (c) 2014年 ___FULLUSERNAME___. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+@property (strong, nonatomic) IBOutlet UIImageView *flower;
+- (IBAction)pinch:(UIPinchGestureRecognizer *)sender;
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+	// Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+- (IBAction)pinch:(UIPinchGestureRecognizer *)sender {
+    
+}
+@end
