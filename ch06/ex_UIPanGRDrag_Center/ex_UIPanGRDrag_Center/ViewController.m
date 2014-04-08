88a7846bc792a38f53940161a60b6e7a80f4b7a4
nomadmonad <nomadmonad@gmail.com>
2014-04-08 14:46:00 +0900

    ビューをドラッグするサンプルを実装

---
+++ ch06/ex_UIPanGRDrag_Center/ex_UIPanGRDrag_Center/ViewController.m
@@ -0,0 +1,33 @@
+//
+//  ViewController.m
+//  ex_UIPanGRDrag_Center
+//
+//  Created by tyamamo on 2014/04/08.
+//  Copyright (c) 2014年 ___FULLUSERNAME___. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+- (IBAction)dragging:(UIPanGestureRecognizer *)sender;
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+	// Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+- (IBAction)dragging:(UIPanGestureRecognizer *)sender {
+    
+}
+@end
