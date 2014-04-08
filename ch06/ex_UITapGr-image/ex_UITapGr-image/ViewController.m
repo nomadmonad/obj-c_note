305479eb22ee7d2e1fd437e81a6b1843b69f2212
nomadmonad <nomadmonad@gmail.com>
2014-04-08 14:24:45 +0900

    UIImageにタップのイベントを追加

--- ch06/ex_UITapGr-image/ex_UITapGr-image/ViewController.m
+++ ch06/ex_UITapGr-image/ex_UITapGr-image/ViewController.m
@@ -9,6 +9,9 @@
 #import "ViewController.h"
 
 @interface ViewController ()
+@property (strong, nonatomic) IBOutlet UIImageView *tombo;
+- (IBAction)tapTombo:(UITapGestureRecognizer *)sender;
+
 
 @end
 
@@ -26,4 +29,16 @@
     // Dispose of any resources that can be recreated.
 }
 
+- (IBAction)tapTombo:(UITapGestureRecognizer *)sender {
+    CGFloat tomboWidth = _tombo.bounds.size.width;
+    CGFloat tomboHeight = _tombo.bounds.size.height;
+    
+    NSInteger w = floor(self.view.bounds.size.width - tomboWidth);
+    NSInteger h = floor(self.view.bounds.size.height - tomboHeight);
+    
+    CGFloat x = (arc4random() % w) + (tomboWidth / 2);
+    CGFloat y = (arc4random() % h) + (tomboHeight / 2);
+    
+    _tombo.center = CGPointMake(x, y);
+}
 @end
