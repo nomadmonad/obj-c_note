072e3343700e7cdded15ce64b2ee39e33bcafa90
nomadmonad <nomadmonad@gmail.com>
2014-04-09 10:09:42 +0900

    シングルタップとダブルタップのイベントを区別して拾うサンプル initial commit

---
+++ ch06/ex_addGestureRecognizer-singleTapDoubleTap/ex_addGestureRecognizer-singleTapDoubleTapTests/ex_addGestureRecognizer_singleTapDoubleTapTests.m
@@ -0,0 +1,34 @@
+//
+//  ex_addGestureRecognizer_singleTapDoubleTapTests.m
+//  ex_addGestureRecognizer-singleTapDoubleTapTests
+//
+//  Created by tyamamo on 2014/04/09.
+//  Copyright (c) 2014年 ___FULLUSERNAME___. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface ex_addGestureRecognizer_singleTapDoubleTapTests : XCTestCase
+
+@end
+
+@implementation ex_addGestureRecognizer_singleTapDoubleTapTests
+
+- (void)setUp
+{
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown
+{
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    XCTFail(@"No implementation for \"%s\"", __PRETTY_FUNCTION__);
+}
+
+@end
