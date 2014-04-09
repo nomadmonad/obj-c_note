bd6ebe2d1b377d8f267b3a24582d8da071efc070
nomadmonad <nomadmonad@gmail.com>
2014-04-09 10:07:23 +0900

    UIGestureRecognizerを使ってタップ時のイベントを拾う処理を実装

--- ch06/ex_addGestureRecognizer/ex_addGestureRecognizer/ViewController.m
+++ ch06/ex_addGestureRecognizer/ex_addGestureRecognizer/ViewController.m
@@ -9,7 +9,10 @@
 #import "ViewController.h"
 
 @interface ViewController ()
-
+{
+    UIImageView *heart;
+}
+- (void) tapHeart:(UIGestureRecognizer *)gesture;
 @end
 
 @implementation ViewController
@@ -18,12 +21,32 @@
 {
     [super viewDidLoad];
 	// Do any additional setup after loading the view, typically from a nib.
+    UIImage *image = [UIImage imageNamed:@"heart.png"];
+    CGFloat width = image.size.width;
+    CGFloat height = image.size.height;
+    CGRect rect = CGRectMake(0, 0, width, height);
+    
+    heart = [[UIImageView alloc] initWithFrame:rect];
+    heart.image = image;
+    heart.contentMode = UIViewContentModeScaleAspectFit;
+    heart.center = CGPointMake(160, 150);
+    heart.userInteractionEnabled = YES;
+    
+    UITapGestureRecognizer *tapGesture = [[UITapGestureRecognizer alloc] initWithTarget:self action:@selector(tapHeart:)];
+    [heart addGestureRecognizer:tapGesture];
+    [self.view addSubview:heart];
 }
 
 - (void)didReceiveMemoryWarning
 {
     [super didReceiveMemoryWarning];
     // Dispose of any resources that can be recreated.
+}
+
+- (void)tapHeart:(UIGestureRecognizer *)gesture
+{
+    NSLog(@"heart tapped.");
+    self.view.backgroundColor = [UIColor purpleColor];
 }
 
 @end
