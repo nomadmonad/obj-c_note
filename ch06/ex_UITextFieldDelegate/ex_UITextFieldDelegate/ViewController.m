6eeef40876998c82261216df762b9fe65c90a31f
nomadmonad <nomadmonad@gmail.com>
2014-04-10 13:19:24 +0900

    UITextfieldにデリゲートを設定するサンプル

---
+++ ch06/ex_UITextFieldDelegate/ex_UITextFieldDelegate/ViewController.m
@@ -0,0 +1,29 @@
+//
+//  ViewController.m
+//  ex_UITextFieldDelegate
+//
+//  Created by tyamamo on 2014/04/10.
+//  Copyright (c) 2014年 ___FULLUSERNAME___. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+	// Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
