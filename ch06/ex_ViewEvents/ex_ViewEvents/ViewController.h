02c4999704fadd9d2a282f1847ca94afacf38c9f
nomadmonad <nomadmonad@gmail.com>
2014-04-10 15:55:31 +0900

    AppDelegateのイベント発生時のログを出力するサンプル

---
+++ ch06/ex_ViewEvents/ex_ViewEvents/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  ex_ViewEvents
+//
+//  Created by tyamamo on 2014/04/10.
+//  Copyright (c) 2014年 ___FULLUSERNAME___. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
