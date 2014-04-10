c86e091b46bb0a9b5f1af153f0d84ae4fc4948cd
nomadmonad <nomadmonad@gmail.com>
2014-04-10 14:35:19 +0900

    UIScrollViewで画像の拡大・縮小とスクロールを実装 Outletの参照元の誤りを修正

--- ch06/ex_UIScrollView/ex_UIScrollView/ViewController.m
+++ ch06/ex_UIScrollView/ex_UIScrollView/ViewController.m
@@ -9,7 +9,12 @@
 #import "ViewController.h"
 
 @interface ViewController ()
+@property (strong, nonatomic) IBOutlet UIScrollView *myScrollView;
+@property (strong, nonatomic) IBOutlet UIImageView *myImageView;
 
+
+
+- (CGRect) zoomRectForScale:(float)scale withCenter:(CGPoint)center;
 @end
 
 @implementation ViewController
@@ -18,12 +23,54 @@
 {
     [super viewDidLoad];
 	// Do any additional setup after loading the view, typically from a nib.
+    _myScrollView.delegate =self;
+    
+    _myScrollView.minimumZoomScale = 1;
+    _myScrollView.maximumZoomScale = 8;
+    
+    _myScrollView.scrollEnabled = YES;
+    _myScrollView.showsHorizontalScrollIndicator = YES;
+    _myScrollView.showsVerticalScrollIndicator = YES;
+    
+    UITapGestureRecognizer *doubleTap = [[UITapGestureRecognizer alloc] initWithTarget:self
+                                                                                action:@selector(doubleTap:)];
+    doubleTap.numberOfTapsRequired = 2;
+    _myImageView.userInteractionEnabled = YES;
+    [_myImageView addGestureRecognizer:doubleTap];
 }
 
 - (void)didReceiveMemoryWarning
 {
     [super didReceiveMemoryWarning];
     // Dispose of any resources that can be recreated.
+}
+
+- (UIView *) viewForZoomingInScrollView:(UIScrollView *)scrollView
+{
+    return _myImageView;
+}
+
+- (void) doubleTap:(UITapGestureRecognizer *) gesture
+{
+    if (_myScrollView.zoomScale < _myScrollView.maximumZoomScale) {
+        float newScale = _myScrollView.zoomScale * 3;
+        CGRect zoomRect = [self zoomRectForScale:newScale withCenter:[gesture locationInView:gesture.view]];
+        [_myScrollView zoomToRect:zoomRect animated:YES];
+    } else {
+        [_myScrollView setZoomScale:1.0 animated:YES];
+    }
+}
+
+- (CGRect)zoomRectForScale:(float)scale withCenter:(CGPoint)center
+{
+    CGRect zoomRect;
+    zoomRect.size.height = [_myScrollView frame].size.height / scale;
+    zoomRect.size.width = [_myScrollView frame].size.width / scale;
+    
+    zoomRect.origin.x = center.x - (zoomRect.size.width / 2.0);
+    zoomRect.origin.y = center.y - (zoomRect.size.height / 2.0);
+    
+    return zoomRect;
 }
 
 @end
