a11183fea24bf1b72d24c3cb32488fd7b2b3b770
nomadmonad <nomadmonad@gmail.com>
2014-04-10 14:42:16 +0900

    ページ送りのサンプル アウトレット接続まで実施

---
+++ ch06/ex_UIScrollView-paging/ex_UIScrollView-paging/ViewController.m
@@ -0,0 +1,31 @@
+//
+//  ViewController.m
+//  ex_UIScrollView-paging
+//
+//  Created by tyamamo on 2014/04/10.
+//  Copyright (c) 2014年 ___FULLUSERNAME___. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+@property (strong, nonatomic) IBOutlet UIScrollView *myScrollView;
+@property (strong, nonatomic) IBOutlet UIPageControl *myPageControl;
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+	// Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
