2ff8e28f0671107fc9e65863f822a1b0d9efab90
nomadmonad <nomadmonad@gmail.com>
2014-04-10 17:14:29 +0900

    UtilityApplicationを使ったセグエのサンプルその２ initial commit

---
+++ ch07/ex_UtlityAppTemplate/ex_UtlityAppTemplateTests/ex_UtlityAppTemplateTests.m
@@ -0,0 +1,34 @@
+//
+//  ex_UtlityAppTemplateTests.m
+//  ex_UtlityAppTemplateTests
+//
+//  Created by tyamamo on 2014/04/10.
+//  Copyright (c) 2014年 ___FULLUSERNAME___. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface ex_UtlityAppTemplateTests : XCTestCase
+
+@end
+
+@implementation ex_UtlityAppTemplateTests
+
+- (void)setUp
+{
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown
+{
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    XCTFail(@"No implementation for \"%s\"", __PRETTY_FUNCTION__);
+}
+
+@end
