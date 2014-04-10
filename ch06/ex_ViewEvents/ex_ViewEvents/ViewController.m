2c514cb1d288082f80ebaa023389f9e4ec90d343
nomadmonad <nomadmonad@gmail.com>
2014-04-10 16:20:43 +0900

    ViewControllerのイベント発生順をログで確認してみた

--- ch06/ex_ViewEvents/ex_ViewEvents/ViewController.m
+++ ch06/ex_ViewEvents/ex_ViewEvents/ViewController.m
@@ -18,12 +18,49 @@
 {
     [super viewDidLoad];
 	// Do any additional setup after loading the view, typically from a nib.
+    NSLog(@"%s", __FUNCTION__);
 }
 
 - (void)didReceiveMemoryWarning
 {
     [super didReceiveMemoryWarning];
     // Dispose of any resources that can be recreated.
+}
+
+- (void) viewWillAppear:(BOOL)animated
+{
+    [super viewWillAppear:animated];
+    NSLog(@"%s", __FUNCTION__);
+}
+
+- (void) viewDidAppear:(BOOL)animated
+{
+    [super viewDidAppear:animated];
+    NSLog(@"%s", __FUNCTION__);
+}
+
+- (void) viewWillDisappear:(BOOL)animated
+{
+    [super viewWillDisappear:animated];
+    NSLog(@"%s", __FUNCTION__);
+}
+
+- (void) viewDidDisappear:(BOOL)animated
+{
+    [super viewDidDisappear:animated];
+    NSLog(@"%s", __FUNCTION__);
+}
+
+- (void) viewWillLayoutSubviews
+{
+    [super viewWillLayoutSubviews];
+    NSLog(@"%s", __FUNCTION__);
+}
+
+- (void) viewDidLayoutSubviews
+{
+    [super viewDidLayoutSubviews];
+    NSLog(@"%s", __FUNCTION__);
 }
 
 @end
