24c5761b90e258d615f31982e587640ed795f387
nomadmonad <nomadmonad@gmail.com>
2014-04-11 11:31:42 +0900

    テーブルビューを使ったサンプル initial commit

---
+++ ch07/ex_MasterDetailApp/ex_MasterDetailApp/DetailViewController.h
@@ -0,0 +1,16 @@
+//
+//  DetailViewController.h
+//  ex_MasterDetailApp
+//
+//  Created by tyamamo on 2014/04/11.
+//  Copyright (c) 2014年 ___FULLUSERNAME___. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface DetailViewController : UIViewController
+
+@property (strong, nonatomic) id detailItem;
+
+@property (weak, nonatomic) IBOutlet UILabel *detailDescriptionLabel;
+@end
