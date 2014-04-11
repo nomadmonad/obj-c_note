c2bbc048a3f4f2b63ad95129a7a125cbdf0567dd
nomadmonad <nomadmonad@gmail.com>
2014-04-11 13:22:29 +0900

    UITableViewを使って指定したリストのURLに飛ぶサンプル

---
+++ ch07/ex_MasterDetailURLList/ex_MasterDetailURLList/MasterViewController.h
@@ -0,0 +1,13 @@
+//
+//  MasterViewController.h
+//  ex_MasterDetailURLList
+//
+//  Created by tyamamo on 2014/04/11.
+//  Copyright (c) 2014年 ___FULLUSERNAME___. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface MasterViewController : UITableViewController
+
+@end
