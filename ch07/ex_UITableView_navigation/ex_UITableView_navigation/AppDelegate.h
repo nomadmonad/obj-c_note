126149559a3337a6a6991abf33416867a5676607
nomadmonad <nomadmonad@gmail.com>
2014-04-11 14:45:16 +0900

    storyboardでビュー関連の項目を作成

---
+++ ch07/ex_UITableView_navigation/ex_UITableView_navigation/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  ex_UITableView_navigation
+//
+//  Created by tyamamo on 2014/04/11.
+//  Copyright (c) 2014年 ___FULLUSERNAME___. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
