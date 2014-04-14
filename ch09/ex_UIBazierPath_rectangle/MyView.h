beeaa01bd1c5c506f7c0526de34d8b7b3a2416b2
nomadmonad <nomadmonad@gmail.com>
2014-04-14 15:25:35 +0900

    描画用のクラスを追加

---
+++ ch09/ex_UIBazierPath_rectangle/MyView.h
@@ -0,0 +1,13 @@
+//
+//  MyView.h
+//  ex_UIBazierPath_rectangle
+//
+//  Created by tyamamo on 2014/04/14.
+//  Copyright (c) 2014年 tyamamo. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface MyView : UIView
+
+@end
---
+++ ch09/ex_UIBazierPath_rectangle/MyView.m
@@ -0,0 +1,31 @@
+//
+//  MyView.m
+//  ex_UIBazierPath_rectangle
+//
+//  Created by tyamamo on 2014/04/14.
+//  Copyright (c) 2014年 tyamamo. All rights reserved.
+//
+
+#import "MyView.h"
+
+@implementation MyView
+
+- (id)initWithFrame:(CGRect)frame
+{
+    self = [super initWithFrame:frame];
+    if (self) {
+        // Initialization code
+    }
+    return self;
+}
+
+/*
+// Only override drawRect: if you perform custom drawing.
+// An empty implementation adversely affects performance during animation.
+- (void)drawRect:(CGRect)rect
+{
+    // Drawing code
+}
+*/
+
+@end
