ebdc888447bfa5b86849789ca035942fe620a215
nomadmonad <nomadmonad@gmail.com>
2014-04-14 13:38:17 +0900

    入力値をUserDefaultsに保存するサンプルを実装

--- ch08/ex_NSUserDefaults/ex_NSUserDefaults/ViewController.m
+++ ch08/ex_NSUserDefaults/ex_NSUserDefaults/ViewController.m
@@ -9,7 +9,14 @@
 #import "ViewController.h"
 
 @interface ViewController ()
+@property (strong, nonatomic) IBOutlet UISlider *mySlider;
+@property (strong, nonatomic) IBOutlet UISwitch *mySwitch;
+@property (strong, nonatomic) IBOutlet UILabel *sliderLabel;
+@property (strong, nonatomic) IBOutlet UILabel *switchLabel;
+- (IBAction)changeSlider:(UISlider *)sender;
+- (IBAction)changeSwitch:(UISwitch *)sender;
 
+- (void)saveDefault;
 @end
 
 @implementation ViewController
@@ -20,10 +27,44 @@
 	// Do any additional setup after loading the view, typically from a nib.
 }
 
+- (void)viewWillAppear:(BOOL)animated
+{
+    NSUserDefaults *myDefaults = [NSUserDefaults standardUserDefaults];
+    _mySlider.value = [myDefaults floatForKey:@"sliderValue"];
+    _sliderLabel.text = [NSString stringWithFormat:@"%2f", _mySlider.value];
+    _mySwitch.on = [myDefaults boolForKey:@"switchValue"];
+    if (_mySwitch.on == YES) {
+        _switchLabel.text = @"ON";
+    } else {
+        _switchLabel.text = @"OFF";
+    }
+}
+
 - (void)didReceiveMemoryWarning
 {
     [super didReceiveMemoryWarning];
     // Dispose of any resources that can be recreated.
 }
 
+- (IBAction)changeSlider:(UISlider *)sender {
+    _sliderLabel.text = [NSString stringWithFormat:@"%2f", sender.value];
+    [self saveDefault];
+}
+
+- (IBAction)changeSwitch:(UISwitch *)sender {
+    if (sender.on == YES) {
+        _switchLabel.text = @"ON";
+    } else {
+        _switchLabel.text = @"OFF";
+    }
+    [self saveDefault];
+}
+
+- (void)saveDefault
+{
+    NSUserDefaults *myDefaults = [NSUserDefaults standardUserDefaults];
+    [myDefaults setFloat:_mySlider.value forKey:@"sliderValue"];
+    [myDefaults setBool:_mySwitch.on forKey:@"switchValue"];
+    [myDefaults synchronize];
+}
 @end
