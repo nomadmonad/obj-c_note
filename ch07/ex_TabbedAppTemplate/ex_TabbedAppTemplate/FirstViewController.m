71270ed43c68fd4bf5195ede2f28f3baff833934
nomadmonad <nomadmonad@gmail.com>
2014-04-14 10:21:48 +0900

    Tabbed Applicationのサンプル initial commit

---
+++ ch07/ex_TabbedAppTemplate/ex_TabbedAppTemplate/FirstViewController.m
@@ -0,0 +1,29 @@
+//
+//  FirstViewController.m
+//  ex_TabbedAppTemplate
+//
+//  Created by tyamamo on 2014/04/14.
+//  Copyright (c) 2014年 ___FULLUSERNAME___. All rights reserved.
+//
+
+#import "FirstViewController.h"
+
+@interface FirstViewController ()
+
+@end
+
+@implementation FirstViewController
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+	// Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
---
+++ ch07/ex_TabbedAppTemplate/ex_TabbedAppTemplate/SecondViewController.h
@@ -0,0 +1,13 @@
+//
+//  SecondViewController.h
+//  ex_TabbedAppTemplate
+//
+//  Created by tyamamo on 2014/04/14.
+//  Copyright (c) 2014年 ___FULLUSERNAME___. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface SecondViewController : UIViewController
+
+@end
