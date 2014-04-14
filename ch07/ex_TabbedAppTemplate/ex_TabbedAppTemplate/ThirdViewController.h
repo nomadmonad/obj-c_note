94a88f37d9652c5bdcec639dec5ccda228aad2cd
nomadmonad <nomadmonad@gmail.com>
2014-04-14 10:35:08 +0900

    3枚目のView controllerとアイコン用の画像を追加

---
+++ ch07/ex_TabbedAppTemplate/ex_TabbedAppTemplate/ThirdViewController.h
@@ -0,0 +1,13 @@
+//
+//  ThirdViewController.h
+//  ex_TabbedAppTemplate
+//
+//  Created by tyamamo on 2014/04/14.
+//  Copyright (c) 2014年 tyamamo. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ThirdViewController : UIViewController
+
+@end
