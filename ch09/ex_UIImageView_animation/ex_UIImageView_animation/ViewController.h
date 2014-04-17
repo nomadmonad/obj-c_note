6b742a5cee64bfd095f34d7c5dbe22d46f9c2f97
nomadmonad <nomadmonad@gmail.com>
2014-04-17 10:21:42 +0900

    png画像を切り替えてアニメーションするサンプル initial commit

---
+++ ch09/ex_UIImageView_animation/ex_UIImageView_animation/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  ex_UIImageView_animation
+//
+//  Created by tyamamo on 2014/04/17.
+//  Copyright (c) 2014年 ___FULLUSERNAME___. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
