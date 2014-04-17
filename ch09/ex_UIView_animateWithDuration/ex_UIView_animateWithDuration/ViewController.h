78a85c0040509cb634ef7c0738fcb8e29b72f654
nomadmonad <nomadmonad@gmail.com>
2014-04-17 10:40:37 +0900

    animateWithDurationで徐々に変わっていくアニメーションのサンプル initial commit

---
+++ ch09/ex_UIView_animateWithDuration/ex_UIView_animateWithDuration/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  ex_UIView_animateWithDuration
+//
+//  Created by tyamamo on 2014/04/17.
+//  Copyright (c) 2014年 ___FULLUSERNAME___. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
