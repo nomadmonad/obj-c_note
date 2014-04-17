606acb36306ade8240b1a3a16b286353aeaff609
nomadmonad <nomadmonad@gmail.com>
2014-04-17 10:04:09 +0900

    tintColorで画像を塗りつぶすサンプルの実装

--- ch09/ex_UIImageView_tintColor/ex_UIImageView_tintColor/ViewController.m
+++ ch09/ex_UIImageView_tintColor/ex_UIImageView_tintColor/ViewController.m
@@ -9,6 +9,12 @@
 #import "ViewController.h"
 
 @interface ViewController ()
+@property (strong, nonatomic) IBOutlet UIImageView *tombo1;
+@property (strong, nonatomic) IBOutlet UIImageView *tombo2;
+@property (strong, nonatomic) IBOutlet UIImageView *tombo3;
+@property (strong, nonatomic) IBOutlet UIImageView *frog;
+@property (strong, nonatomic) IBOutlet UIImageView *flower;
+@property (strong, nonatomic) IBOutlet UIImageView *car;
 
 @end
 
@@ -17,7 +23,18 @@
 - (void)viewDidLoad
 {
     [super viewDidLoad];
-	// Do any additional setup after loading the view, typically from a nib.
+	_tombo1.image = [_tombo1.image imageWithRenderingMode:UIImageRenderingModeAlwaysTemplate];
+    _tombo2.image = [_tombo2.image imageWithRenderingMode:UIImageRenderingModeAlwaysTemplate];
+    _tombo1.tintColor = [UIColor colorWithRed:0.000 green:0.8 blue:0.6 alpha:1.0];
+    _tombo2.tintColor = [UIColor colorWithRed:0 green:0.3 blue:0.5 alpha:1.0];
+    
+    _tombo3.image = [_tombo3.image imageWithRenderingMode:UIImageRenderingModeAlwaysTemplate];
+    _frog.image = [_frog.image imageWithRenderingMode:UIImageRenderingModeAlwaysTemplate];
+    
+    _flower.image = [_flower.image imageWithRenderingMode:UIImageRenderingModeAlwaysTemplate];
+    _car.image = [_car.image imageWithRenderingMode:UIImageRenderingModeAlwaysTemplate];
+    _flower.tintColor = [UIColor blueColor];
+    _car.tintColor = [UIColor redColor];
 }
 
 - (void)didReceiveMemoryWarning
