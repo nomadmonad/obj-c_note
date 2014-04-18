3f72242914d6e0fd5277602bfdbedc95493f2c68
nomadmonad <nomadmonad@gmail.com>
2014-04-18 09:36:04 +0900

    スプリングのように減衰するアニメーション initial commit

---
+++ ch09/ex_UIView_animateWithDuration_spring/ex_UIView_animateWithDuration_spring/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  ex_UIView_animateWithDuration_spring
+//
+//  Created by tyamamo on 2014/04/18.
+//  Copyright (c) 2014年 ___FULLUSERNAME___. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ ch09/ex_UIView_animateWithDuration_spring/ex_UIView_animateWithDuration_spring/ViewController.m
@@ -0,0 +1,34 @@
+//
+//  ViewController.m
+//  ex_UIView_animateWithDuration_spring
+//
+//  Created by tyamamo on 2014/04/18.
+//  Copyright (c) 2014年 ___FULLUSERNAME___. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+- (IBAction)tapView:(UITapGestureRecognizer *)sender;
+
+@end
+
+@implementation ViewController
+
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+	// Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+- (IBAction)tapView:(UITapGestureRecognizer *)sender
+{
+}
+@end
