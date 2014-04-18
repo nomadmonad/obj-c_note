61d13b135aca4466735af778c55d83eb68507533
nomadmonad <nomadmonad@gmail.com>
2014-04-18 10:07:47 +0900

    トランジションのサンプル initial commit

--- ch09/ex_UIView_animateWithDuration_spring/ex_UIView_animateWithDuration_spring/ViewController.m
+++ ch09/ex_UIView_animateWithDuration_spring/ex_UIView_animateWithDuration_spring/ViewController.m
@@ -9,6 +9,7 @@
 #import "ViewController.h"
 
 @interface ViewController ()
+@property (weak, nonatomic) IBOutlet UIView *myBox;
 - (IBAction)tapView:(UITapGestureRecognizer *)sender;
 
 @end
@@ -30,5 +31,26 @@
 
 - (IBAction)tapView:(UITapGestureRecognizer *)sender
 {
+    CGPoint tappedPoint = [sender locationInView:self.view];
+    
+    CGFloat r = (arc4random() % 100) / 100.0;
+    CGFloat g = (arc4random() % 100) / 100.0;
+    CGFloat b = (arc4random() % 100) / 100.0;
+    UIColor *boxColor = [UIColor colorWithRed:r green:g blue:b alpha:1.0];
+    
+    CGFloat width = (arc4random() % 100) + 50;
+    CGPoint origin = CGPointMake(tappedPoint.x - (width / 2), tappedPoint.y - (width / 2));
+    CGRect destFrame = CGRectMake(origin.x, origin.y, width, width);
+    
+    [UIView animateWithDuration:1.0
+                          delay:0.0
+         usingSpringWithDamping:0.2
+          initialSpringVelocity:5.0
+                        options:0
+                     animations:^{
+                         _myBox.frame = destFrame;
+                         _myBox.backgroundColor = boxColor;
+                     }
+                     completion:nil];
 }
 @end
---
+++ ch09/ex_UIView_transition/ex_UIView_transition/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  ex_UIView_transition
+//
+//  Created by tyamamo on 2014/04/18.
+//  Copyright (c) 2014年 ___FULLUSERNAME___. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
