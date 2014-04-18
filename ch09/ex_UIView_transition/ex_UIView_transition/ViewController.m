bad0f2fbbcdfca34b8b75ffdce8335ac38f58ddf
nomadmonad <nomadmonad@gmail.com>
2014-04-18 10:25:01 +0900

    3秒おきに画像が切り替わるサンプル

--- ch09/ex_UIView_transition/ex_UIView_transition/ViewController.m
+++ ch09/ex_UIView_transition/ex_UIView_transition/ViewController.m
@@ -9,6 +9,16 @@
 #import "ViewController.h"
 
 @interface ViewController ()
+{
+    NSTimer *timer;
+    NSArray *photoNames;
+    int photoIndex;
+}
+@property (weak, nonatomic) IBOutlet UIImageView *myPhoto;
+@property (weak, nonatomic) IBOutlet UILabel *caption;
+@property (weak, nonatomic) IBOutlet UIPickerView *transitionPicker;
+
+- (void)goNextPhoto;
 
 @end
 
@@ -17,13 +27,48 @@
 - (void)viewDidLoad
 {
     [super viewDidLoad];
-	// Do any additional setup after loading the view, typically from a nib.
+	
+    photoNames = @[@"funaya.jpg", @"kabe.jpg", @"uminoie.jpg"];
+    photoIndex = 0;
+    
+    _caption.text = photoNames[photoIndex];
+    
+    timer = [NSTimer scheduledTimerWithTimeInterval:3
+                                             target:self
+                                           selector:@selector(goNextPhoto)
+                                           userInfo:nil
+                                            repeats:YES];
 }
 
 - (void)didReceiveMemoryWarning
 {
     [super didReceiveMemoryWarning];
     // Dispose of any resources that can be recreated.
+}
+
+- (void)goNextPhoto
+{
+    if (photoIndex < (photoNames.count - 1)) {
+        photoIndex++;
+    } else {
+        photoIndex = 0;
+    }
+    
+    UIImageView *fromView = _myPhoto;
+    
+    UIImage *nextPhoto = [UIImage imageNamed:photoNames[photoIndex]];
+    UIImageView *toView = [[UIImageView alloc] initWithImage:nextPhoto];
+    
+    toView.frame = fromView.frame;
+    
+    [UIView transitionFromView:fromView
+                        toView:toView
+                      duration:1.0
+                       options:UIViewAnimationOptionTransitionCurlUp
+                    completion:nil];
+    
+    _myPhoto = toView;
+    _caption.text = photoNames[photoIndex];
 }
 
 @end
