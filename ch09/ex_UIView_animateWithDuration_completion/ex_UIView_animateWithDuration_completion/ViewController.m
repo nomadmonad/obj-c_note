8f386ac6784a6fb203ef91f0ac293e28794e3c2b
nomadmonad <nomadmonad@gmail.com>
2014-04-18 09:27:47 +0900

    AutoLayoutを有効にし、アニメーションが狙い通りに動くよう変更

--- ch09/ex_UIView_animateWithDuration_completion/ex_UIView_animateWithDuration_completion/ViewController.m
+++ ch09/ex_UIView_animateWithDuration_completion/ex_UIView_animateWithDuration_completion/ViewController.m
@@ -9,6 +9,9 @@
 #import "ViewController.h"
 
 @interface ViewController ()
+@property (weak, nonatomic) IBOutlet UIImageView *tomboImage;
+
+- (IBAction)tapView:(UITapGestureRecognizer *)sender;
 
 @end
 
@@ -17,13 +20,69 @@
 - (void)viewDidLoad
 {
     [super viewDidLoad];
-	// Do any additional setup after loading the view, typically from a nib.
+    _tomboImage.center = [self randamPoint];
 }
 
 - (void)didReceiveMemoryWarning
 {
     [super didReceiveMemoryWarning];
     // Dispose of any resources that can be recreated.
+}
+
+- (IBAction)tapView:(UITapGestureRecognizer *)sender {
+    CGPoint tapPoint = [sender locationInView:self.view];
+    CGFloat angle = atan2(tapPoint.y - _tomboImage.center.y,
+                          tapPoint.x - _tomboImage.center.x);
+    _tomboImage.transform = CGAffineTransformMakeRotation(angle);
+    
+    UIViewAnimationOptions options = UIViewAnimationOptionBeginFromCurrentState
+                            | UIViewAnimationOptionAllowUserInteraction
+                            | UIViewAnimationOptionCurveEaseOut;
+    
+    [UIView animateWithDuration:1.0
+                          delay:0.0 options:options
+                     animations:^{
+                         _tomboImage.center = tapPoint;
+                     }
+                     completion:^(BOOL finished) {
+                         [self getOut:_tomboImage];
+                     }];
+    
+}
+
+- (void) getOut:(UIImageView *)view
+{
+    UIViewAnimationOptions options = UIViewAnimationOptionBeginFromCurrentState
+    | UIViewAnimationOptionAllowUserInteraction
+    | UIViewAnimationOptionCurveEaseIn;
+    
+    [UIView animateWithDuration:0.5
+                          delay:1.0
+                        options:options
+                     animations:^{
+                         CGPoint newPoint = [self randamPoint];
+                         CGFloat newAngle = atan2(newPoint.y - _tomboImage.center.y,
+                                                  newPoint.x - _tomboImage.center.x);
+                         _tomboImage.transform = CGAffineTransformMakeRotation(newAngle);
+                         _tomboImage.center = newPoint;
+                     }
+                     completion:nil];
+}
+
+- (CGPoint)randamPoint
+{
+    int height = self.view.bounds.size.height + 100;
+    int y = arc4random() % height - 50;
+    
+    CGPoint pt;
+    int isLeft = arc4random() % 2;
+    if (isLeft) {
+        pt = CGPointMake(-100.0, y);
+    } else {
+        pt = CGPointMake(self.view.bounds.size.width + 100.0, y);
+    }
+    
+    return pt;
 }
 
 @end
