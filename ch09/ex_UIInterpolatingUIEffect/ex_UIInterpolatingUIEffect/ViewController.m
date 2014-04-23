e9c6c6d36128bd2556f44a75c35a8098e3907f2c
nomadmonad <nomadmonad@gmail.com>
2014-04-23 16:48:36 +0900

    モーションエフェクトを使ったアニメーションの実装まで完了

--- ch09/ex_UIInterpolatingUIEffect/ex_UIInterpolatingUIEffect/ViewController.m
+++ ch09/ex_UIInterpolatingUIEffect/ex_UIInterpolatingUIEffect/ViewController.m
@@ -9,6 +9,7 @@
 #import "ViewController.h"
 
 @interface ViewController ()
+@property (strong, nonatomic) IBOutletCollection(UIImageView) NSArray *trees;
 
 @end
 
@@ -17,13 +18,42 @@
 - (void)viewDidLoad
 {
     [super viewDidLoad];
-	// Do any additional setup after loading the view, typically from a nib.
+	self.view.backgroundColor = [UIColor colorWithPatternImage:[UIImage imageNamed: @"stars.png"]];
+    
+    [self ipMotionEffect:_trees[0] offset:10.0];
+    [self ipMotionEffect:_trees[1] offset:50.0];
+    [self ipMotionEffect:_trees[2] offset:50.0];
+    [self ipMotionEffect:_trees[3] offset:90.0];
+    [self ipMotionEffect:_trees[4] offset:90.0];
+    [self ipMotionEffect:_trees[5] offset:90.0];
 }
 
 - (void)didReceiveMemoryWarning
 {
     [super didReceiveMemoryWarning];
     // Dispose of any resources that can be recreated.
+}
+
+- (void)ipMotionEffect:(UIView *)theView offset:(CGFloat) offset
+{
+    UIInterpolatingMotionEffect *effectX =
+        [[UIInterpolatingMotionEffect alloc]
+         initWithKeyPath:@"center.x"
+                    type:UIInterpolatingMotionEffectTypeTiltAlongHorizontalAxis];
+    
+    UIInterpolatingMotionEffect *effectY =
+        [[UIInterpolatingMotionEffect alloc]
+         initWithKeyPath:@"center.y"
+                    type:UIInterpolatingMotionEffectTypeTiltAlongVerticalAxis];
+    
+    effectX.maximumRelativeValue = @(offset);
+    effectX.minimumRelativeValue = @(-offset);
+    
+    effectY.maximumRelativeValue = @(offset);
+    effectY.minimumRelativeValue = @(-offset);
+    
+    [theView addMotionEffect:effectX];
+    [theView addMotionEffect:effectY];
 }
 
 @end
