674d5b2b8b9f75f48fa2b941cbcefc479da7f25e
nomadmonad <nomadmonad@gmail.com>
2014-04-23 16:50:23 +0900

    UIDynamicAnimatorのサンプル

---
+++ ch09/ex_UIDA_Gravity_Collision/ex_UIDA_Gravity_Collision/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  ex_UIDA_Gravity_Collision
+//
+//  Created by tyamamo on 2014/04/23.
+//  Copyright (c) 2014年 ___FULLUSERNAME___. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
---
+++ ch09/ex_UIDA_Gravity_Collision/ex_UIDA_Gravity_Collision/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  ex_UIDA_Gravity_Collision
+//
+//  Created by tyamamo on 2014/04/23.
+//  Copyright (c) 2014年 ___FULLUSERNAME___. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
