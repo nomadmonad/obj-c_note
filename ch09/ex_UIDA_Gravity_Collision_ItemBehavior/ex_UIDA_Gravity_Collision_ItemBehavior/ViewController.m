f720304111f627ac1cd04d997730993241b7b525
nomadmonad <nomadmonad@gmail.com>
2014-04-24 16:30:42 +0900

    カエルとアザラシが衝突するサンプル

--- ch09/ex_UIDA_Gravity_Collision_ItemBehavior/ex_UIDA_Gravity_Collision_ItemBehavior/ViewController.m
+++ ch09/ex_UIDA_Gravity_Collision_ItemBehavior/ex_UIDA_Gravity_Collision_ItemBehavior/ViewController.m
@@ -9,7 +9,13 @@
 #import "ViewController.h"
 
 @interface ViewController ()
+- (IBAction)tappedView:(UITapGestureRecognizer *)sender;
 
+@property (nonatomic, strong) UIDynamicAnimator* animator;
+@property (nonatomic, strong) UIDynamicItemBehavior* frogItemBehavior;
+@property (nonatomic, strong) UIDynamicItemBehavior* sealItemBehavior;
+@property (nonatomic, strong) UIGravityBehavior* gravityBehavior;
+@property (nonatomic, strong) UICollisionBehavior* collisionBehavior;
 @end
 
 @implementation ViewController
@@ -17,7 +23,30 @@
 - (void)viewDidLoad
 {
     [super viewDidLoad];
-	// Do any additional setup after loading the view, typically from a nib.
+	
+    self.animator = [[UIDynamicAnimator alloc] initWithReferenceView:self.view];
+    
+    NSArray* items = nil;
+    self.frogItemBehavior = [[UIDynamicItemBehavior alloc] initWithItems:items];
+    self.frogItemBehavior.density = 1.0;
+    self.frogItemBehavior.elasticity = 0.6;
+    self.frogItemBehavior.friction = 0.3;
+    self.frogItemBehavior.resistance = 0.5;
+    
+    self.sealItemBehavior = [[UIDynamicItemBehavior alloc] initWithItems:items];
+    self.sealItemBehavior.density = 0.5;
+    self.sealItemBehavior.elasticity = 1.0;
+    self.sealItemBehavior.friction = 0.5;
+    self.sealItemBehavior.resistance = 0.2;
+    
+    self.gravityBehavior = [[UIGravityBehavior alloc] initWithItems:items];
+    self.collisionBehavior = [[UICollisionBehavior alloc] initWithItems:items];
+    self.collisionBehavior.translatesReferenceBoundsIntoBoundary = YES;
+    
+    [self.animator addBehavior:self.frogItemBehavior];
+    [self.animator addBehavior:self.sealItemBehavior];
+    [self.animator addBehavior:self.gravityBehavior];
+    [self.animator addBehavior:self.collisionBehavior];
 }
 
 - (void)didReceiveMemoryWarning
@@ -26,4 +55,25 @@
     // Dispose of any resources that can be recreated.
 }
 
+- (IBAction)tappedView:(UITapGestureRecognizer *)sender {
+    NSArray* itemNames = @[@"frog.png", @"azarashi.png"];
+    NSInteger index = arc4random() % itemNames.count;
+    NSString* imageName = itemNames[index];
+    
+    UIImage* image = [UIImage imageNamed:imageName];
+    UIImageView* imageView = [[UIImageView alloc] initWithImage:image];
+    
+    imageView.center = [sender locationInView:self.view];
+    [self.view addSubview:imageView];
+    
+    if ([imageName isEqualToString:@"frog.png"]) {
+        [self.frogItemBehavior addItem:imageView];
+    } else {
+        [self.sealItemBehavior addItem:imageView];
+    }
+    
+    [self.gravityBehavior addItem:imageView];
+    [self.collisionBehavior addItem:imageView];
+    
+}
 @end
