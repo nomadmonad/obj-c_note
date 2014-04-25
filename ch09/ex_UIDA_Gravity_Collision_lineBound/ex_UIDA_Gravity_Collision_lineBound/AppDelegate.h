28146fb2e14b81ba4dd9b30c3a4fc7b015d5d0fe
nomadmonad <nomadmonad@gmail.com>
2014-04-25 10:03:03 +0900

    ImageViewが障害物にぶつかって反射するサンプル

---
+++ ch09/ex_UIDA_Gravity_Collision_lineBound/ex_UIDA_Gravity_Collision_lineBound/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  ex_UIDA_Gravity_Collision_lineBound
+//
+//  Created by tyamamo on 2014/04/24.
+//  Copyright (c) 2014年 ___FULLUSERNAME___. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ ch09/ex_UIDA_Gravity_Collision_lineBound/ex_UIDA_Gravity_Collision_lineBound/ViewController.m
@@ -0,0 +1,29 @@
+//
+//  ViewController.m
+//  ex_UIDA_Gravity_Collision_lineBound
+//
+//  Created by tyamamo on 2014/04/24.
+//  Copyright (c) 2014年 ___FULLUSERNAME___. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+	// Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
