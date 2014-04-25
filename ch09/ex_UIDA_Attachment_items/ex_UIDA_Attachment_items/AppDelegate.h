32a7e908effd4a7cc9ed4c0ea690a5a88e42cec8
nomadmonad <nomadmonad@gmail.com>
2014-04-25 17:06:07 +0900

    アタッチメントビヘイビアのサンプル

---
+++ ch09/ex_UIDA_Attachment_items/ex_UIDA_Attachment_items/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  ex_UIDA_Attachment_items
+//
+//  Created by tyamamo on 2014/04/25.
+//  Copyright (c) 2014年 ___FULLUSERNAME___. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
