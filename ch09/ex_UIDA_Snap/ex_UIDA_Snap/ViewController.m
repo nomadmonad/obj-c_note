28e8d5f56683e040aa8ca873e465e897db7c27be
nomadmonad <nomadmonad@gmail.com>
2014-04-25 17:03:59 +0900

    目玉がスナップするサンプル

--- ch09/ex_UIDA_Snap/ex_UIDA_Snap/ViewController.m
+++ ch09/ex_UIDA_Snap/ex_UIDA_Snap/ViewController.m
@@ -9,6 +9,20 @@
 #import "ViewController.h"
 
 @interface ViewController ()
+{
+    CGFloat offsetRx, offsetRy;
+    CGFloat offsetLx, offsetLy;
+}
+@property (weak, nonatomic) IBOutlet UIImageView* frogBody;
+@property (weak, nonatomic) IBOutlet UIImageView* frogLeftEye;
+@property (weak, nonatomic) IBOutlet UIImageView* frogRightEye;
+
+@property (nonatomic, strong) UIDynamicAnimator* animator;
+@property (nonatomic, strong) UISnapBehavior* bodySnapBehavior;
+@property (nonatomic, strong) UISnapBehavior* rightEyeSnapBehavior;
+@property (nonatomic, strong) UISnapBehavior* leftEyeSnapBehavior;
+
+- (IBAction)tappedView:(UITapGestureRecognizer *)sender;
 
 @end
 
@@ -17,7 +31,12 @@
 - (void)viewDidLoad
 {
     [super viewDidLoad];
-	// Do any additional setup after loading the view, typically from a nib.
+
+    _animator = [[UIDynamicAnimator alloc] initWithReferenceView:self.view];
+    offsetRx = _frogRightEye.center.x - _frogBody.center.x;
+    offsetRy = _frogRightEye.center.y - _frogBody.center.y;
+    offsetLx = _frogLeftEye.center.x - _frogBody.center.x;
+    offsetLy = _frogLeftEye.center.x - _frogBody.center.y;
 }
 
 - (void)didReceiveMemoryWarning
@@ -26,4 +45,21 @@
     // Dispose of any resources that can be recreated.
 }
 
+- (IBAction)tappedView:(UITapGestureRecognizer *)sender {
+    CGPoint tappedPoint = [sender locationInView:self.view];
+    CGPoint eyeRPoint = CGPointMake(tappedPoint.x + offsetRx, tappedPoint.y + offsetRy);
+    CGPoint eyeLPoint = CGPointMake(tappedPoint.x + offsetLx, tappedPoint.y + offsetLy);
+    
+    [_animator removeBehavior:_bodySnapBehavior];
+    [_animator removeBehavior:_rightEyeSnapBehavior];
+    [_animator removeBehavior:_leftEyeSnapBehavior];
+    
+    _bodySnapBehavior = [[UISnapBehavior alloc] initWithItem:_frogBody snapToPoint:tappedPoint];
+    _rightEyeSnapBehavior = [[UISnapBehavior alloc] initWithItem:_frogRightEye snapToPoint:eyeRPoint];
+    _leftEyeSnapBehavior = [[UISnapBehavior alloc] initWithItem:_frogLeftEye snapToPoint:eyeLPoint];
+    
+    [_animator addBehavior:_bodySnapBehavior];
+    [_animator addBehavior:_rightEyeSnapBehavior];
+    [_animator addBehavior:_leftEyeSnapBehavior];
+}
 @end
