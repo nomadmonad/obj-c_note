a932e4346d95aad5d0368d3c1e8a6a38f249e9ce
nomadmonad <nomadmonad@gmail.com>
2014-04-25 16:28:11 +0900

    カエルを打ち出すサンプルの実装

--- ch09/ex_UIDA_Gravity_Collision_Push/ex_UIDA_Gravity_Collision_Push/ViewController.m
+++ ch09/ex_UIDA_Gravity_Collision_Push/ex_UIDA_Gravity_Collision_Push/ViewController.m
@@ -9,7 +9,14 @@
 #import "ViewController.h"
 
 @interface ViewController ()
-
+{
+    NSTimer* timerObj;
+}
+@property (nonatomic, strong) UIDynamicAnimator* animator;
+@property (nonatomic, strong) UIDynamicItemBehavior* itemBehavior;
+@property (nonatomic, strong) UIGravityBehavior* gravityBehavior;
+@property (nonatomic, strong) UICollisionBehavior* collisionBehavior;
+@property (nonatomic, strong) UIPushBehavior* pushBehavior;
 @end
 
 @implementation ViewController
@@ -17,7 +24,34 @@
 - (void)viewDidLoad
 {
     [super viewDidLoad];
-	// Do any additional setup after loading the view, typically from a nib.
+    
+    NSArray* items = nil;
+    
+    _itemBehavior = [[UIDynamicItemBehavior alloc] initWithItems:items];
+    _itemBehavior.density = 1.0;
+    _itemBehavior.elasticity = 0.3;
+    _itemBehavior.friction = 0.5;
+    _itemBehavior.resistance = 0.8;
+    
+    _gravityBehavior = [[UIGravityBehavior alloc] initWithItems:items];
+    
+    _collisionBehavior = [[UICollisionBehavior alloc] initWithItems:items];
+    _collisionBehavior.translatesReferenceBoundsIntoBoundary = YES;
+    
+    _pushBehavior = [[UIPushBehavior alloc]initWithItems:items mode:UIPushBehaviorModeInstantaneous];
+    
+    _animator = [[UIDynamicAnimator alloc] initWithReferenceView:self.view];
+    [_animator addBehavior:_itemBehavior];
+    [_animator addBehavior:_gravityBehavior];
+    [_animator addBehavior:_collisionBehavior];
+    [_animator addBehavior:_pushBehavior];
+    
+    timerObj = [NSTimer scheduledTimerWithTimeInterval:1.5
+                                                target:self
+                                              selector:@selector(addFrog)
+                                              userInfo:(nil)
+                                               repeats:YES];
+    
 }
 
 - (void)didReceiveMemoryWarning
@@ -26,4 +60,29 @@
     // Dispose of any resources that can be recreated.
 }
 
+- (void)addFrog
+{
+    UIImage* image = [UIImage imageNamed:@"frog.png"];
+    UIImageView* imageView = [[UIImageView alloc] initWithImage:image];
+    CGFloat y = self.view.frame.size.height;
+    imageView.center = CGPointMake(50, y - 100);
+    [self.view addSubview:imageView];
+    
+    if (_pushBehavior.items.count > 0) {
+        [_pushBehavior removeItem:_pushBehavior.items[0]];
+        _pushBehavior.active = YES;
+    }
+    
+    _pushBehavior.angle = -M_PI / 4;
+    _pushBehavior.magnitude = 1 * ((arc4random() % 10) * 0.1);
+    
+    [_itemBehavior addItem:imageView];
+    [_gravityBehavior addItem:imageView];
+    [_collisionBehavior addItem:imageView];
+    [_pushBehavior addItem:imageView];
+    
+    if (_itemBehavior.items.count >= 10) {
+        [timerObj invalidate];
+    }
+}
 @end
