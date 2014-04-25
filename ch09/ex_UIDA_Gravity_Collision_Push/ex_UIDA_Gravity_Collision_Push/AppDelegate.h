54274d85d1168b557ea43a0dcd32d334207a1a46
nomadmonad <nomadmonad@gmail.com>
2014-04-25 16:08:30 +0900

    PushBehaviorのサンプル initial commit

---
+++ ch09/ex_UIDA_Gravity_Collision_Push/ex_UIDA_Gravity_Collision_Push/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  ex_UIDA_Gravity_Collision_Push
+//
+//  Created by tyamamo on 2014/04/25.
+//  Copyright (c) 2014年 ___FULLUSERNAME___. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
