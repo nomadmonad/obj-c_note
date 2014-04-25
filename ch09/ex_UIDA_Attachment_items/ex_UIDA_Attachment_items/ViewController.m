e3878fc5e477da738c46f255b81cbee35d75796c
nomadmonad <nomadmonad@gmail.com>
2014-04-25 17:22:34 +0900

    AtttachmentBehaviorでView同士を連結

--- ch09/ex_UIDA_Attachment_items/ex_UIDA_Attachment_items/ViewController.m
+++ ch09/ex_UIDA_Attachment_items/ex_UIDA_Attachment_items/ViewController.m
@@ -9,7 +9,14 @@
 #import "ViewController.h"
 
 @interface ViewController ()
+@property (weak, nonatomic) IBOutlet UIImageView *tShirt;
+@property (weak, nonatomic) IBOutlet UIImageView *frog;
 
+@property (nonatomic, strong) UIDynamicAnimator* animator;
+@property (nonatomic, strong) UIDynamicItemBehavior* itemBehavior;
+@property (nonatomic, strong) UIGravityBehavior* gravityBahivor;
+@property (nonatomic, strong) UICollisionBehavior* collisionBehavior;
+@property (nonatomic, strong) UIAttachmentBehavior* attachmentBehavior;
 @end
 
 @implementation ViewController
@@ -17,7 +24,31 @@
 - (void)viewDidLoad
 {
     [super viewDidLoad];
-	// Do any additional setup after loading the view, typically from a nib.
+
+    _animator = [[UIDynamicAnimator alloc] initWithReferenceView:self.view];
+    
+    NSArray* items = @[_tShirt];
+    _itemBehavior = [[UIDynamicItemBehavior alloc] initWithItems:items];
+    _itemBehavior.density = 1.0;
+    _itemBehavior.elasticity = 0.8;
+    _itemBehavior.friction = 0.6;
+    _itemBehavior.resistance = 0.5;
+    
+    _gravityBahivor = [[UIGravityBehavior alloc] initWithItems:items];
+    
+    _collisionBehavior = [[UICollisionBehavior alloc] initWithItems:items];
+    _collisionBehavior.translatesReferenceBoundsIntoBoundary = YES;
+    
+    _attachmentBehavior = [[UIAttachmentBehavior alloc] initWithItem:(id<UIDynamicItem>)_frog
+                                                    attachedToItem:(id<UIDynamicItem>)_tShirt];
+    _attachmentBehavior.damping = 0.1;
+    _attachmentBehavior.length = 1.0;
+    _attachmentBehavior.frequency = 6;
+    
+    [_animator addBehavior:_itemBehavior];
+    [_animator addBehavior:_gravityBahivor];
+    [_animator addBehavior:_collisionBehavior];
+    [_animator addBehavior:_attachmentBehavior];
 }
 
 - (void)didReceiveMemoryWarning
