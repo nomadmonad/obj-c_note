e5daa04247e6411902aa86046e11cc86543de98d
nomadmonad <nomadmonad@gmail.com>
2014-04-30 15:15:48 +0900

    デバイスの表・裏の変化に応じて画像を補正するサンプルを実装

--- ch10/ex_faceUpDown_fadeInOut/ex_faceUpDown_fadeInOut/ViewController.m
+++ ch10/ex_faceUpDown_fadeInOut/ex_faceUpDown_fadeInOut/ViewController.m
@@ -9,7 +9,11 @@
 #import "ViewController.h"
 
 @interface ViewController ()
+@property (weak, nonatomic) IBOutlet UIImageView *bgImage;
+@property (weak, nonatomic) IBOutlet UIImageView *tombo;
 
+- (void) changeDeviceOrientation:(id)sender;
+- (void) fadeInOut:(UIImageView *)imageView alpha:(float)alpnaV;
 @end
 
 @implementation ViewController
@@ -17,7 +21,45 @@
 - (void)viewDidLoad
 {
     [super viewDidLoad];
-	// Do any additional setup after loading the view, typically from a nib.
+	_tombo.alpha = 0;
+    
+    [[NSNotificationCenter defaultCenter] addObserver:self
+                                             selector:@selector(changeDeviceOrientation:)
+                                                 name:UIDeviceOrientationDidChangeNotification
+                                               object:nil];
+}
+
+- (void)changeDeviceOrientation:(id)sender
+{
+    switch ([[UIDevice currentDevice] orientation]) {
+        case UIDeviceOrientationFaceUp:
+            [self fadeInOut:_tombo alpha:0];
+            [self fadeInOut:_bgImage alpha:1.0];
+            break;
+        case UIDeviceOrientationFaceDown:
+            [self fadeInOut:_tombo alpha:1.0];
+            [self fadeInOut:_bgImage alpha:0.2];
+            break;
+        default:
+            break;
+    }
+}
+
+- (void) fadeInOut:(UIImageView *)imageView alpha:(float)alpnaV
+{
+    
+    UIViewAnimationOptions options =
+        UIViewAnimationOptionBeginFromCurrentState
+        | UIViewAnimationOptionAllowUserInteraction
+        | UIViewAnimationOptionCurveEaseInOut;
+    
+    [UIView animateWithDuration:0.5
+                          delay:0.0
+                        options:options
+                     animations:^{
+                         imageView.alpha = alpnaV;
+                     }
+                     completion:nil];
 }
 
 - (void)didReceiveMemoryWarning
