5106cc4b565cd5edb242325e355c97775cba18bd
nomadmonad <nomadmonad@gmail.com>
2014-04-30 17:22:43 +0900

    加速度センサの値をプッシュ方式で取得するサンプル

--- ch10/ex_CMMotionManager_acceleroMeter_pull/ex_CMMotionManager_acceleroMeter_pull/ViewController.m
+++ ch10/ex_CMMotionManager_acceleroMeter_pull/ex_CMMotionManager_acceleroMeter_pull/ViewController.m
@@ -7,8 +7,19 @@
 //
 
 #import "ViewController.h"
+#import <CoreMotion/CoreMotion.h>
 
 @interface ViewController ()
+{
+    CMMotionManager* motionManager;
+}
+@property (weak, nonatomic) IBOutlet UILabel *labelX;
+@property (weak, nonatomic) IBOutlet UILabel *labelY;
+@property (weak, nonatomic) IBOutlet UILabel *labelZ;
+@property (weak, nonatomic) IBOutlet UISwitch *mySwitch;
+- (IBAction)startStop:(UISwitch *)sender;
+@property (weak, nonatomic) IBOutlet UIButton *updateButton;
+- (IBAction)update:(UIButton *)sender;
 
 @end
 
@@ -26,4 +37,26 @@
     // Dispose of any resources that can be recreated.
 }
 
+- (IBAction)startStop:(UISwitch *)sender {
+    if (_mySwitch.on) {
+        if (!motionManager) {
+            motionManager = [[CMMotionManager alloc] init];
+        }
+        [motionManager startAccelerometerUpdates];
+        _updateButton.hidden = NO;
+    } else {
+        [motionManager stopAccelerometerUpdates];
+        _updateButton.hidden = YES;
+    }
+}
+- (IBAction)update:(UIButton *)sender {
+    CMAccelerometerData* data = motionManager.accelerometerData;
+    double accX = data.acceleration.x;
+    double accY = data.acceleration.y;
+    double accZ = data.acceleration.z;
+    
+    _labelX.text = [NSString stringWithFormat:@"x = %.4f", accX];
+    _labelY.text = [NSString stringWithFormat:@"y = %.4f", accY];
+    _labelZ.text = [NSString stringWithFormat:@"z = %.4f", accZ];
+}
 @end
