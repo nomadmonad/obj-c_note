d55d69b13514c4b423ed0960446ae6f20eb999cb
nomadmonad <nomadmonad@gmail.com>
2014-04-30 10:06:12 +0900

    デバイスか移転のサンプル

---
+++ ch10/ex_shouldAutoRotate/ex_shouldAutoRotate/MainViewController.m
@@ -0,0 +1,43 @@
+//
+//  MainViewController.m
+//  ex_shouldAutoRotate
+//
+//  Created by tyamamo on 2014/04/30.
+//  Copyright (c) 2014年 ___FULLUSERNAME___. All rights reserved.
+//
+
+#import "MainViewController.h"
+
+@interface MainViewController ()
+
+@end
+
+@implementation MainViewController
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+	// Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+#pragma mark - Flipside View
+
+- (void)flipsideViewControllerDidFinish:(FlipsideViewController *)controller
+{
+    [self dismissViewControllerAnimated:YES completion:nil];
+}
+
+- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender
+{
+    if ([[segue identifier] isEqualToString:@"showAlternate"]) {
+        [[segue destinationViewController] setDelegate:self];
+    }
+}
+
+@end
