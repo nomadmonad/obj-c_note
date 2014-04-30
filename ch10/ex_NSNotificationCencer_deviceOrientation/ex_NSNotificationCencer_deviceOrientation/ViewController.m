82b6f6eab92628a462b00ee53b53467beebc0da7
nomadmonad <nomadmonad@gmail.com>
2014-04-30 14:57:43 +0900

    NSNotoficationCendterを使っでデバイスの回転を検知するサンプルの実装完了 デバイスの裏表を拾って画像を変化させるサンプル

--- ch10/ex_NSNotificationCencer_deviceOrientation/ex_NSNotificationCencer_deviceOrientation/ViewController.m
+++ ch10/ex_NSNotificationCencer_deviceOrientation/ex_NSNotificationCencer_deviceOrientation/ViewController.m
@@ -9,6 +9,10 @@
 #import "ViewController.h"
 
 @interface ViewController ()
+@property (weak, nonatomic) IBOutlet UILabel *labelA;
+@property (weak, nonatomic) IBOutlet UILabel *labelB;
+@property (weak, nonatomic) IBOutlet UILabel *labelC;
+@property (weak, nonatomic) IBOutlet UILabel *labelD;
 
 @end
 
@@ -17,13 +21,52 @@
 - (void)viewDidLoad
 {
     [super viewDidLoad];
-	// Do any additional setup after loading the view, typically from a nib.
+    NSNotificationCenter *notification = [NSNotificationCenter defaultCenter];
+    [notification addObserver:self
+                     selector:@selector(changeDeviceOrientation:)
+                         name:UIDeviceOrientationDidChangeNotification
+                       object:nil];
 }
 
 - (void)didReceiveMemoryWarning
 {
     [super didReceiveMemoryWarning];
     // Dispose of any resources that can be recreated.
+}
+
+- (void)changeDeviceOrientation:(id)sender
+{
+    UIDevice* device = [UIDevice currentDevice];
+    
+    switch (device.orientation) {
+        case UIDeviceOrientationPortrait:
+            [self rotateLabels:0];
+            break;
+        case UIDeviceOrientationPortraitUpsideDown:
+            [self rotateLabels:180];
+            break;
+        case UIDeviceOrientationLandscapeLeft:
+            [self rotateLabels:90];
+            break;
+        case UIDeviceOrientationLandscapeRight:
+            [self rotateLabels:-90];
+            break;
+        default:
+            NSLog(@"%1d", device.orientation);
+            break;
+    }
+}
+
+- (void)rotateView:(UIView*)targetView degree:(float)deg
+{
+    targetView.transform = CGAffineTransformMakeRotation((deg * M_PI) / 180.0);
+}
+- (void)rotateLabels:(float)degree
+{
+    [self rotateView:_labelA degree:degree];
+    [self rotateView:_labelB degree:degree];
+    [self rotateView:_labelC degree:degree];
+    [self rotateView:_labelD degree:degree];
 }
 
 @end
