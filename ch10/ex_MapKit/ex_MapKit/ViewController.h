babb7c6a188ed4b572ee7183c3204fe0e3019775
nomadmonad <nomadmonad@gmail.com>
2014-05-01 15:31:30 +0900

    地図表示のサンプル実装

---
+++ ch10/ex_MapKit/ex_MapKit/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  ex_MapKit
+//
+//  Created by tyamamo on 2014/05/01.
+//  Copyright (c) 2014年 ___FULLUSERNAME___. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
---
+++ ch10/ex_MapKit/ex_MapKit/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  ex_MapKit
+//
+//  Created by tyamamo on 2014/05/01.
+//  Copyright (c) 2014年 ___FULLUSERNAME___. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
