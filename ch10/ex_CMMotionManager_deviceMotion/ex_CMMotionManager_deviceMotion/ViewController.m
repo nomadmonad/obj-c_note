f65138aec142f6d16f3f6a6fd47ccf26441961eb
nomadmonad <nomadmonad@gmail.com>
2014-05-01 14:02:57 +0900

    加速度センサから速度や傾きを取得するサンプルを実装

--- ch10/ex_CMMotionManager_deviceMotion/ex_CMMotionManager_deviceMotion/ViewController.m
+++ ch10/ex_CMMotionManager_deviceMotion/ex_CMMotionManager_deviceMotion/ViewController.m
@@ -7,9 +7,14 @@
 //
 
 #import "ViewController.h"
+#import <CoreMotion/CoreMotion.h>
 
 @interface ViewController ()
-
+{
+    CMMotionManager* motionManager;
+}
+@property (weak, nonatomic) IBOutlet UILabel *myLabel;
+@property (weak, nonatomic) IBOutlet UIImageView *ufo;
 @end
 
 @implementation ViewController
@@ -17,7 +22,7 @@
 - (void)viewDidLoad
 {
     [super viewDidLoad];
-	// Do any additional setup after loading the view, typically from a nib.
+	[self startAnimation];
 }
 
 - (void)didReceiveMemoryWarning
@@ -26,4 +31,38 @@
     // Dispose of any resources that can be recreated.
 }
 
+- (void)startAnimation
+{
+    motionManager = [[CMMotionManager alloc] init];
+    
+    if (!motionManager.deviceMotionAvailable) {
+        return;
+    }
+    motionManager.deviceMotionUpdateInterval = 0.006;
+    CMDeviceMotionHandler handler = ^(CMDeviceMotion* motion, NSError* error){
+        [self animation:motion];
+    };
+    [motionManager startDeviceMotionUpdatesToQueue:[NSOperationQueue currentQueue]
+                                       withHandler:handler];
+    
+}
+
+- (void)animation:(CMDeviceMotion*)motion
+{
+    double time = motion.timestamp;
+    double roll = motion.attitude.roll;
+    double pitch = motion.attitude.pitch;
+    double yaw = motion.attitude.yaw;
+    
+    double width = self.view.bounds.size.width;
+    double height = self.view.bounds.size.height;
+    double cX = width * roll / M_PI;
+    double cY = height * pitch /M_PI;
+    
+    CGAffineTransform tf1 = CGAffineTransformMakeRotation(yaw);
+    CGAffineTransform tf2 = CGAffineTransformMakeTranslation(cX, cY);
+    
+    _ufo.transform = CGAffineTransformConcat(tf1, tf2);
+    _myLabel.text = [NSString stringWithFormat:@"%.2f", time];
+}
 @end
