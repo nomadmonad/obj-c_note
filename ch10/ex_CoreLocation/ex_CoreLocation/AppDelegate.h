d84539894c4857398fb069879a1a2cd8bd83832c
nomadmonad <nomadmonad@gmail.com>
2014-05-01 14:03:56 +0900

    CoreLocationを使ったサンプル

---
+++ ch10/ex_CoreLocation/ex_CoreLocation/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  ex_CoreLocation
+//
+//  Created by tyamamo on 2014/05/01.
+//  Copyright (c) 2014年 ___FULLUSERNAME___. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
