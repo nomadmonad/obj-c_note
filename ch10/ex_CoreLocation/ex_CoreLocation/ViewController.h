2e7285fe364a1ffc22f315b99d9bed8a2ca0c4e3
nomadmonad <nomadmonad@gmail.com>
2014-05-01 15:27:41 +0900

    CoreLocationを使った位置情報取得のサンプルを実装

--- ch10/ex_CoreLocation/ex_CoreLocation/ViewController.h
+++ ch10/ex_CoreLocation/ex_CoreLocation/ViewController.h
@@ -7,7 +7,12 @@
 //
 
 #import <UIKit/UIKit.h>
+#import <CoreLocation/CoreLocation.h>
 
-@interface ViewController : UIViewController
+@interface ViewController : UIViewController <CLLocationManagerDelegate>
+{
+    CLLocationManager* locationManager;
+    BOOL useNorrh;
+}
 
 @end
--- ch10/ex_CoreLocation/ex_CoreLocation/ViewController.m
+++ ch10/ex_CoreLocation/ex_CoreLocation/ViewController.m
@@ -9,6 +9,15 @@
 #import "ViewController.h"
 
 @interface ViewController ()
+@property (weak, nonatomic) IBOutlet UILabel *latitude;
+@property (weak, nonatomic) IBOutlet UILabel *gratitude;
+@property (weak, nonatomic) IBOutlet UILabel *height;
+@property (weak, nonatomic) IBOutlet UILabel *declination;
+@property (weak, nonatomic) IBOutlet UILabel *orientation;
+@property (weak, nonatomic) IBOutlet UISwitch *magNorthSwitch;
+- (IBAction)changedMagNorthSwitch:(UISwitch *)sender;
+
+@property (weak, nonatomic) IBOutlet UIImageView *connpass;
 
 @end
 
@@ -17,7 +26,7 @@
 - (void)viewDidLoad
 {
     [super viewDidLoad];
-	// Do any additional setup after loading the view, typically from a nib.
+	[self startLocationServices];
 }
 
 - (void)didReceiveMemoryWarning
@@ -26,4 +35,56 @@
     // Dispose of any resources that can be recreated.
 }
 
+- (IBAction)changedMagNorthSwitch:(UISwitch *)sender {
+    useNorrh = sender.on;
+}
+
+- (void)startLocationServices
+{
+    if(!locationManager) {
+        locationManager = [[CLLocationManager alloc] init];
+        locationManager.delegate = self;
+    }
+    
+    if([CLLocationManager locationServicesEnabled]) {
+        locationManager.desiredAccuracy = kCLLocationAccuracyBest;
+        locationManager.distanceFilter = 3;
+        [locationManager startUpdatingLocation];
+    }
+    
+    if ([CLLocationManager headingAvailable]) {
+        useNorrh = NO;
+        _magNorthSwitch.on = NO;
+        locationManager.headingOrientation = CLDeviceOrientationPortrait;
+        locationManager.headingFilter = 1;
+        [locationManager startUpdatingHeading];
+    }
+}
+
+- (void)locationManager:(CLLocationManager *)manager didUpdateLocations:(NSArray *)locations
+{
+    CLLocation* locationData = [locations lastObject];
+    _latitude.text = [NSString stringWithFormat:@"%.3f", locationData.coordinate.latitude];
+    _gratitude.text = [NSString stringWithFormat:@"%.3f", locationData.coordinate.longitude];
+    _height.text = [NSString stringWithFormat:@"%.3f", locationData.altitude];
+}
+
+- (void)locationManager:(CLLocationManager *)manager didUpdateHeading:(CLHeading *)newHeading
+{
+    CLLocationDirection trueNorth = newHeading.trueHeading;
+    CLLocationDirection magNorth = newHeading.magneticHeading;
+    
+    _declination.text = [NSString stringWithFormat:@"%.1f", trueNorth - magNorth];
+    
+    CLLocationDirection heading;
+    if (useNorrh) {
+        
+        heading = magNorth;
+    } else {
+        heading = trueNorth;
+    }
+    
+    _connpass.transform = CGAffineTransformMakeRotation(-heading * M_PI / 180);
+    _orientation.text = [NSString stringWithFormat:@"%.1f", heading];
+}
 @end
