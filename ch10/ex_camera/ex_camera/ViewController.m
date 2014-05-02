89530dadc42dc6fb2ade499a9dde9c006c3065a2
nomadmonad <nomadmonad@gmail.com>
2014-05-02 11:16:13 +0900

    UIImagePickerを使って画像を編集して保存するサンプル

--- ch10/ex_camera/ex_camera/ViewController.m
+++ ch10/ex_camera/ex_camera/ViewController.m
@@ -8,7 +8,10 @@
 
 #import "ViewController.h"
 
-@interface ViewController ()
+@interface ViewController () <UIImagePickerControllerDelegate, UINavigationControllerDelegate>
+@property (weak, nonatomic) IBOutlet UIImageView *myImageView;
+@property (weak, nonatomic) IBOutlet UISwitch *originalSaveSwitch;
+- (IBAction)startCamera:(UIBarButtonItem *)sender;
 
 @end
 
@@ -24,6 +27,40 @@
 {
     [super didReceiveMemoryWarning];
     // Dispose of any resources that can be recreated.
+}
+
+- (IBAction)startCamera:(UIBarButtonItem *)sender {
+    if ([UIImagePickerController isSourceTypeAvailable:UIImagePickerControllerSourceTypeCamera]) {
+        UIImagePickerController *imagePicker = [[UIImagePickerController alloc] init];
+        imagePicker.delegate = self;
+        imagePicker.sourceType = UIImagePickerControllerSourceTypeCamera;
+        imagePicker.allowsEditing = YES;
+        
+        [self presentViewController:imagePicker
+                           animated:YES
+                         completion:nil];
+    }
+}
+
+- (void)imagePickerControllerDidCancel:(UIImagePickerController *)picker
+{
+    [self dismissViewControllerAnimated:YES
+                             completion:nil];
+}
+
+- (void)imagePickerController:(UIImagePickerController *)picker didFinishPickingMediaWithInfo:(NSDictionary *)info
+{
+    UIImage* originalImage = (UIImage *)[info objectForKey:UIImagePickerControllerOriginalImage];
+    UIImage* editedImage = (UIImage *)[info objectForKey:UIImagePickerControllerEditedImage];
+    _myImageView.image = editedImage;
+    
+    UIImageWriteToSavedPhotosAlbum(editedImage, nil, nil, nil);
+    
+    if (_originalSaveSwitch.on) {
+        UIImageWriteToSavedPhotosAlbum(originalImage, nil, nil, nil);
+    }
+    [self dismissViewControllerAnimated:YES
+                             completion:nil];
 }
 
 @end
