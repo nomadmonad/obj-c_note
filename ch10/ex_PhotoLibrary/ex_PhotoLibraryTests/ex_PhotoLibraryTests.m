f917a9972950a4887e070dc65ed1598907c3fa75
nomadmonad <nomadmonad@gmail.com>
2014-05-02 11:41:04 +0900

    Photo Libraryから画像を取得するサンプル

---
+++ ch10/ex_PhotoLibrary/ex_PhotoLibraryTests/ex_PhotoLibraryTests.m
@@ -0,0 +1,34 @@
+//
+//  ex_PhotoLibraryTests.m
+//  ex_PhotoLibraryTests
+//
+//  Created by tyamamo on 2014/05/02.
+//  Copyright (c) 2014年 ___FULLUSERNAME___. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface ex_PhotoLibraryTests : XCTestCase
+
+@end
+
+@implementation ex_PhotoLibraryTests
+
+- (void)setUp
+{
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown
+{
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    XCTFail(@"No implementation for \"%s\"", __PRETTY_FUNCTION__);
+}
+
+@end
