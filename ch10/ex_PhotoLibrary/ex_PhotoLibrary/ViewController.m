a70fa2502f84581da452adbbfae268f83631ed56
nomadmonad <nomadmonad@gmail.com>
2014-05-02 12:11:27 +0900

    Photo Albumの画像を合成するサンプルを実装

--- ch10/ex_PhotoLibrary/ex_PhotoLibrary/ViewController.m
+++ ch10/ex_PhotoLibrary/ex_PhotoLibrary/ViewController.m
@@ -29,6 +29,13 @@
 }
 
 - (IBAction)selsectPhoto:(UIBarButtonItem *)sender {
+    if ([UIImagePickerController isSourceTypeAvailable:UIImagePickerControllerSourceTypePhotoLibrary]) {
+        UIImagePickerController* picker = [[UIImagePickerController alloc] init];
+        picker.delegate = self;
+        picker.allowsEditing = YES;
+        picker.sourceType = UIImagePickerControllerSourceTypePhotoLibrary;
+        [self presentViewController:picker animated:YES completion:nil];
+    }
 }
 
 -(void) imagePickerController:(UIImagePickerController *)picker didFinishPickingMediaWithInfo:(NSDictionary *)info
@@ -38,7 +45,24 @@
     
     CGSize theSize = CGSizeMake(640, 640);
     CGRect theRect = CGRectMake(0, 0, 640, 640);
+    float posX = (theSize.width - editedImage.size.width) / 2;
+    float posY = (theSize.height - editedImage.size.height) / 2;
     
+    UIGraphicsBeginImageContext(theSize);
+    CGContextRef context = UIGraphicsGetCurrentContext();
+    CGContextStrokeRect(context, theRect);
+    CGContextSetRGBFillColor(context, 0.0, 0.0, 0.0, 1.0);
+    CGContextFillRect(context, theRect);
+    [editedImage drawInRect:CGRectMake(posX, posY, editedImage.size.width, editedImage.size.height)];
+    
+    [sunImage drawInRect:CGRectMake(0, 0, sunImage.size.width, sunImage.size.height)];
+    
+    UIImage* result = UIGraphicsGetImageFromCurrentImageContext();
+    
+    UIGraphicsEndImageContext();
+    UIImageWriteToSavedPhotosAlbum(result, nil, nil, nil);
+    _myImageView.image = result;
+    [self dismissViewControllerAnimated:YES completion:nil];
 }
 
 -(void) imagePickerControllerDidCancel:(UIImagePickerController *)picker
